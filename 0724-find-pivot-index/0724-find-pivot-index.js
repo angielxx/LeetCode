89e8547c2780792ef6d358dbc82786ae5e067b42
이은지 / Angie Lee <angielxx94@gmail.com>
2022-10-29 18:39:09 +0900

    Time: 2769 ms (5.02%), Space: 79.1 MB (5.07%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var pivotIndex = function(nums) {
+    let pivot;
+    for (let i = 0; i < nums.length; i++) {
+        const left = nums.slice(0, i);
+        const right = nums.slice(i+1, nums.length);
+        const leftSum = left.reduce((pre, curr) => pre + curr, 0);
+        const rightSum = right.reduce((pre, curr) => pre + curr, 0);
+        console.log(i, leftSum, rightSum)
+        if (leftSum === rightSum) {
+            pivot = i;
+            break;
+        }
+    }
+    if (pivot != undefined) {
+        return pivot;
+    } else {
+        return -1;
+    }
+};
